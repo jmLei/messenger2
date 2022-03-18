d1cb10d57e2c194681f2d23b7b91897558426060
andrewShinjo <shinjo1233@gmail.com>
2022-03-17 17:40:50 -0700

    Implemented GET request for reading specific user

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 Node.js
 - mongoose package used to connect to interact with MongoDB
+
+# API
+
+GET    /user      (Retrieves all users)
+POST   /user      (Create a user)
+GET    /user/{id} (Retrieves a user with id = {id})
+PUT    /user/{id} (Update a user with id = {id})
+DELETE /user/{id} (Delete a user with id = {id})
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const http = require("http")         // Importing http library
-const mongoose = require("mongoose") // Importing mongoose library
-const User = require("./models/user") // Importing user schema
-const port = 8080
+const http = require("http");          // Importing http library
+const mongoose = require("mongoose");  // Importing mongoose library
+const User = require("./models/user"); // Importing user schema
+const port = 8080;
 
 // Connects to MongoDB database named "messenger"
 // located at localhost:27017
@@ -9,32 +9,44 @@ mongoose.connect("mongodb://localhost:27017/messenger")
 const db = mongoose.connection
 
 db.on("error", (error) => {
-    console.log("Error: ", error)
-})
+    console.log("Error: ", error);
+});
 
 db.once("open", () => {
-    console.log("Connected to MongoDB database.")
-})
+    console.log("Connected to MongoDB database.");
+});
 
 const server = http.createServer((req, res) => {
 
     // Routes
-    const url = req.url
-    const method = req.method
+    const url = req.url.split("/");
 
-    if(url === "/user" && method === "GET") {
+    const method = req.method;
+    const resource = (1 < url.length) ? url[1] : "";
+    const id = (2 < url.length) ? url[2] : "";
 
-        console.log("GET /user")
+    // GET localhost:8080/user => Read all users
+    if(method === "GET" && resource === "users" && id === "") {
+        console.log("GET /users");
 
         User.find().then((users) => {
+            res.writeHead(200, {"Content-Type": "application/json"});
+            res.write(JSON.stringify(users));
+            res.end();
+        });
+    } 
+    // GET localhost:8080/user/{id} => Read a user where id = {id}
+    else if(method === "GET" && resource === "users" && id !== "") {
+        console.log(`GET users/${id}`);
 
-            res.writeHead(200, {"Content-Type": "application/json"})
-            res.write(JSON.stringify(users))
-            res.end()
-
+        User.findById(id).then((user) => {
+            res.writeHead(200, {"Content-Type": "application/json"});
+            res.write(JSON.stringify(user));
+            res.end();
         })
-
-    } else if(url === "/user" && method === "POST") {
+    }
+    // POST localhost:8080/user => Create a new user
+    else if(method === "POST" && resource === "users") {
 
         console.log("POST /user")
 
@@ -64,12 +76,6 @@ const server = http.createServer((req, res) => {
         }).on("error", (error) => {
             console.log("Error: ", error)
         })
-
-    } else {
-
-        res.writeHead(404, {"Content-Type": "text/html"})
-        res.write("No route found.")
-        res.end()
 
     }
 
